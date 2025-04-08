844baf923fec7b96c57e3a3a74f66db6605d0f35
andreslopezco <fandreslopezramirez@gmail.com>
2025-04-08 13:44:34 -0500

    add dotenv config to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
