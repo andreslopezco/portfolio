946e9279e7c3c9153d59b7d54e8c2ee8f4f1690d
andreslopezco <fandreslopezramirez@gmail.com>
2025-04-10 21:21:00 -0500

    Update tsparticles

--- src/components/react/Particles.tsx
+++ src/components/react/Particles.tsx
@@ -1,31 +1,98 @@
-import { useCallback, useEffect, useState } from "react";
-import type { Container, Engine } from "tsparticles-engine";
-import { loadSlim } from "tsparticles-slim";
-
-let ReactParticles: any = null;
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { type Container, type ISourceOptions } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesComponent = () => {
-  const [isClient, setIsClient] = useState(false);
+  const [init, setInit] = useState(false);
 
   useEffect(() => {
-    const loadParticles = async () => {
-      const { Particles } = await import("react-tsparticles");
-      ReactParticles = Particles;
-      setIsClient(true);
-    };
-    loadParticles();
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  const particlesInit = useCallback(async (engine: Engine) => {
-    await loadSlim(engine);
-  }, []);
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
 
-  if (!isClient || !ReactParticles) return null;
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fullScreen: false,
+      background: {
+        color: {
+          value: "transparent",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          repulse: {
+            distance: 100,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#94a3b8",
+        },
+        links: {
+          color: "#94a3b8",
+          distance: 150,
+          enable: true,
+          opacity: 0.2,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.2,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (!init) {
+    return <></>;
+  }
 
   return (
-    <ReactParticles
+    <Particles
       id="tsparticles"
-      init={particlesInit}
+      particlesLoaded={particlesLoaded}
+      options={options}
       style={{
         width: "100%",
         height: "100%",
@@ -34,68 +101,6 @@ const ParticlesComponent = () => {
         left: 0,
         right: 0,
         bottom: 0,
-      }}
-      options={{
-        fullScreen: false,
-        background: {
-          color: {
-            value: "transparent",
-          },
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-          },
-          modes: {
-            repulse: {
-              distance: 100,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#94a3b8",
-          },
-          links: {
-            color: "#94a3b8",
-            distance: 150,
-            enable: true,
-            opacity: 0.2,
-            width: 1,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: true,
-            speed: 1,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.2,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 3 },
-          },
-        },
-        detectRetina: true,
       }}
     />
   );
